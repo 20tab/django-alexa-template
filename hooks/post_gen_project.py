e730d7c0b9b89ee27b71c8753f841f04d7a3aa48
Raffaele Colace <raffaele@20tab.com>
2019-09-05 15:32:34 +0200

    .env tpl file added

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@ import os
 
 
 def create_virtualenv():
+    os.system("cp .env.tpl .env")
     os.mkdir(".venv")
     os.system("virtualenv .venv/skill_env")
     os.system(".venv/skill_env/bin/pip install -r requirements.txt")
